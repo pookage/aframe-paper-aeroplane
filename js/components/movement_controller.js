65ae685d6ca65758d6951589beba818990a47ded
pookage <pookagehayes@gmail.com>
2017-11-18 21:23:10 +0000

    correctly flies in the direction it's pointing - but input now needs to add to rotation instead of setting it

--- js/components/movement_controller.js
+++ js/components/movement_controller.js
@@ -10,8 +10,6 @@ AFRAME.registerComponent("movement_controller", {
 	//-----------------------------------------
 	init: function(){
 
-		const element    = this.el;
-
 		//scope binding
 		this.updateSpeed = this.updateSpeed.bind(this);
 		this.updateRoll  = this.updateRoll.bind(this);
@@ -19,17 +17,28 @@ AFRAME.registerComponent("movement_controller", {
 		this.updateYaw   = this.updateYaw.bind(this);
 
 		//setup
-		this.markers     = {
-			front: this.createPositionMarker("front"),
-			rear: this.createPositionMarker("rear")
-		}
-		for(let marker in this.markers) element.appendChild(this.markers[marker]);
+		this.markers     = this.setupOrientationMarkers();
+		this.speedVector = new THREE.Vector3(0, 0, 0);
+
 		this.addListeners();
 
 	},//init
 	remove: function(){
 		this.removeListeners();
 	},//remove
+	tick: function(){
+
+		const element          = this.el;
+		const directionVector  = this.calculateDirectionVector();
+		const {x, y, z}        = element.getAttribute("position");
+		const currentPosition  = new THREE.Vector3(x, y, z);
+		const nextPositionStep = directionVector.multiply(this.speedVector);
+		const newPosition      = currentPosition.add(nextPositionStep);
+
+		element.setAttribute("position", newPosition)
+
+
+	},//tick
 	
 
 	//EVENT MANAGEMENT
@@ -71,9 +80,67 @@ AFRAME.registerComponent("movement_controller", {
 		element.removeEventListener("joystick-yaw-updated", this.updateYaw)
 	},//removeJoystickListeners
 
+
 	///////////////////////////////////////////	
 	//HANDLING/////////////////////////////////
 	///////////////////////////////////////////
+	updateSpeed: function(event){
+		const element = this.el;
+		const speed   = event.detail.value;
+		const clampedSpeed = (speed + 1) / 2;
+
+		this.speedVector = new THREE.Vector3(clampedSpeed, clampedSpeed, clampedSpeed)
+
+		element.emit("speed-updated", { value: clampedSpeed });
+		//console.log("updating speed to : ", speed)
+	},//updateSpeed
+	updateRoll: function(event){
+		const element      = this.el;
+		const inputScalar  = event.detail.value * 180 || 0;
+		const { x, y, z }  = element.getAttribute("rotation");
+		const rollRotation = 1 * inputScalar
+		const newRotation  = new THREE.Vector3(x, y, rollRotation); //use later for iterative slow increases
+
+		element.setAttribute("rotation", newRotation)
+	},//updateRoll
+	updatePitch: function(event){
+		const element = this.el;
+
+		const inputScalar   = event.detail.value * 180 || 0;
+		const { x, y, z }   = element.getAttribute("rotation");
+		const pitchRotation = 1 * inputScalar
+		const newRotation   = new THREE.Vector3(pitchRotation, y, z); //use later for iterative slow increases
+
+		element.setAttribute("rotation", newRotation)
+	},//updatePitch
+	updateYaw: function(event){
+		const element = this.el;
+
+		const inputScalar   = event.detail.value * 180 || 0;
+		const { x, y, z }   = element.getAttribute("rotation");
+		const yawRotation = (1 * inputScalar);
+		const newRotation   = new THREE.Vector3(x, yawRotation, z); //use later for iterative slow increases
+
+		element.setAttribute("rotation", newRotation)
+	},//updateYaw
+
+
+	//UTILITY FUNCTIONS
+	//----------------------------------------------
+	setupOrientationMarkers: function(){
+		const element     = this.el;
+		const frontMarker = this.createPositionMarker("front");
+		const rearMarker  = this.createPositionMarker("rear");
+		const markers = [
+			frontMarker,
+			rearMarker
+		];
+		for(let marker of markers) element.appendChild(marker);
+		return {
+			front: frontMarker,
+			rear: rearMarker
+		};
+	},//setupOrientationMarkers
 	createPositionMarker: function(type){
 		const marker = document.createElement("a-sphere");
 		let color, position, scale;
@@ -92,42 +159,12 @@ AFRAME.registerComponent("movement_controller", {
 		marker.setAttribute("scale", "0.25 0.25 0.25");
 		return marker;
 	},//createPositionMarker
-	updateSpeed: function(event){
-		const element = this.el;
-		const speed   = event.detail.value;
-		const clampedSpeed = (speed + 1) / 2;
+	calculateDirectionVector: function(){
 
-		element.emit("speed-updated", { value: clampedSpeed });
-		//console.log("updating speed to : ", speed)
-	},//updateSpeed
-	updateRoll: function(event){
-		const element      = this.el;
-		const inputScalar  = event.detail.value * 90 || 0;
-		const { x, y, z }  = element.getAttribute("rotation");
-		const rollRotation = 1 * inputScalar
-		const newRotation  = new THREE.Vector3(x, y, rollRotation); //use later for iterative slow increases
-
-		element.setAttribute("rotation", newRotation)
-	},//updateRoll
-	updatePitch: function(event){
-		const element = this.el;
-
-		const inputScalar   = event.detail.value * 90 || 0;
-		const { x, y, z }   = element.getAttribute("rotation");
-		const pitchRotation = 1 * inputScalar
-		const newRotation   = new THREE.Vector3(pitchRotation, y, z); //use later for iterative slow increases
-
-		element.setAttribute("rotation", newRotation)
-	},//updatePitch
-	updateYaw: function(event){
-		const element = this.el;
-
-		const inputScalar   = event.detail.value * 90 || 0;
-		const { x, y, z }   = element.getAttribute("rotation");
-		const yawRotation = (1 * inputScalar);
-		const newRotation   = new THREE.Vector3(x, yawRotation, z); //use later for iterative slow increases
-
-		element.setAttribute("rotation", newRotation)
-	},//updateYaw
+		const frontPosition = this.markers.front.object3D.getWorldPosition();
+		const rearPosition  = this.markers.rear.object3D.getWorldPosition();
+		const direction     = frontPosition.sub(rearPosition).normalize();
+		return direction;
+	},//calculateDirectionVector
 
 });
